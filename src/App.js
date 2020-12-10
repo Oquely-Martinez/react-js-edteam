422505db7639a4c478e45f42d7f3abe6b1191f48
Darwin Martinez <darwin.oquely@yahoo.com>
2020-12-10 16:46:01 -0600

    codigo de la clase3

--- src/App.js
+++ src/App.js
@@ -2,7 +2,31 @@ import React, {Fragment} from "react";
 import Curso from './Curso'
 import './styles/styles.scss'
 
-
+const cursos = [
+  {
+    title: "Klotin desde cero",
+    image: "https://edteam-media.s3.amazonaws.com/courses/medium/d793ee5a-9dd1-4a3a-9c82-9b08a5f89a52.png",
+    price: "20 USD",
+    profesor: "José Dimas Luján"
+  }, 
+  {
+    title: "Html Canvas",
+    image: "https://edteam-media.s3.amazonaws.com/courses/big/c574aade-f5b4-42ae-9d92-6b05e7919f4d.png",
+    price: "30 USD",
+    profesor: "Diego Adrian"
+  }, 
+  {
+    title: "Mi primer arquitectura AWS",
+    image: "https://edteam-media.s3.amazonaws.com/courses/big/dd7bcebe-4e99-4b50-a75a-ff47eb6dad1a.png",
+    price: "25 USD",
+    profesor: "Gerardo Castro"
+  },
+  {
+    title: "Linux desde cero",
+    image: "https://edteam-media.s3.amazonaws.com/courses/medium/5ffc4c77-cbc3-476d-9c18-e180882a52c9.jpg",
+    price: "15 USD",
+    profesor: "Diego Bastidas"}
+]
 
 const App = ()=>(
 
@@ -21,12 +45,25 @@ const App = ()=>(
       </div>
 
       <div className="ed-grid s-grid-3">
+      <Curso 
+        image="https://edteam-media.s3.amazonaws.com/specialities/medium/a2058c16-73f8-4ea4-ad16-5f9bcbae5147.jpg" 
+        title="PHP Programador Backend" 
+        price="$20 USD"
+        profesor="Darwin Martinez"/>
+
         <Curso />
-        <Curso />
-        <Curso />
-        <Curso />
-        <Curso />
-        <Curso />
+        
+
+        {
+          cursos.map(curso=>
+            <Curso 
+            title={curso.title} 
+            image={curso.image}
+            price={curso.price}
+            profesor={curso.profesor} />
+          )
+        }
+        
       </div>
 
 
--- src/Curso.jsx
+++ src/Curso.jsx
@@ -1,31 +1,42 @@
 import React from 'react'
 import './styles/styles.scss'
+import PropTypes from 'prop-types'
 
-const Curso = ()=>(
+
+
+
+const Curso = ({image, title, price, profesor})=>(
     <article className="card">
           <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
-            <img src="https://edteam-media.s3.amazonaws.com/courses/medium/61e5a210-8dab-412e-a6dc-802c070cc18c.jpg" alt=""/> 
+            <img src={image} alt=""/> 
           </div>
           <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
             <h3 className="t5 s-mb-2 s-center">
-              Javascript Desde Cero
+                {title}
             </h3>
-            <div className="s-mb-2 s-main-center">
-              <div className="card__teacher s-cross-center">
-                <div className="card__avatar s-mr-1">
-                  <div className="circle img-container">
-                    <img src="https://edteam-media.s3.amazonaws.com/users/thumbnail/c2be732e-7341-49ea-8f49-6cc461c42ca4.png" alt=""/>
-                  </div>
-                </div>
-                <span className="small">Alexys Lozada</span>
-              </div>
-            </div>
             <div className="s-main-center">
-              <a className="button--ghost-alert button--tiny" href="#">$ 20USD</a>
+                {`Profe.: ${profesor}`}
             </div>
+                <div className="s-main-center">
+                <a className="button--ghost-alert button--tiny" href="#">$ {price}</a>
+                </div>
           </div>
         </article>
 
 )
+
+Curso.propTypes = {
+    title: PropTypes.string,
+    image: PropTypes.string,
+    price: PropTypes.string,
+    profesor: PropTypes.string
+}
+
+Curso.defaultProps ={
+    title:"No hay titulo",
+    image:"https://edteam-media.s3.amazonaws.com/specialities/medium/fd2746a1-413d-4435-a084-24493adbd20b.png",
+    price: "$ 0.00 USD",
+    profesor: "No tiene profesor"
+}
 
 export default Curso
